8c5301340629e3f95030dab3a379e9d539a5872d
ManarAhmedFawzy <manar-ahmed96@hotmail.com>
2019-04-04 15:27:17 +0200

    conflict fixed

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -48,11 +48,8 @@ namespace WebApplication2
                 .AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-<<<<<<< HEAD
 
-=======
             services.AddScoped<ApplicationDbContext, ApplicationDbContext>();
->>>>>>> e9e66357204fd5acd3a80d63ff855a4f9b89649c
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             //services.AddAuthentication().AddFacebook(f => { f.AppId = "797388533951711"; f.AppSecret = "a9314c03cc8cdd7d24673006cde25b7c"; });
 
