57e8da84f22659dd9e0558fd0cf6ba229e891d98
ManarAhmedFawzy <manar-ahmed96@hotmail.com>
2019-04-21 18:46:46 +0200

    user

--- WebApplication2/Controllers/ProfileController.cs
+++ WebApplication2/Controllers/ProfileController.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
 using WebApplication2.Services;
 
 namespace WebApplication2.Controllers
 {
     public class ProfileController : Controller
     {
-        private  ProfileService service { get; }
-        public ProfileController(ProfileService s)
+        private readonly ProfileService service;
+        private readonly UserManager<ApplicationUser> user;
+
+        public ProfileController(ProfileService s , UserManager<ApplicationUser>u)
         {
             service = s;
+            user = u;
         }
         public IActionResult Index()
         {
+       
+          
             return View();
         }
     }
